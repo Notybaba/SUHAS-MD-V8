e6f5da34f9766036b7c7a0f9c981be69f7c3e97c
Notybaba <dinuvarachamod@gmail.com>
2024-12-31 13:08:34 +0530

    💕𝚂 𝚄 𝙷 𝙰 𝚂 - 𝙼 𝙳🧚‍♂️

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const { File } = require('megajs')
 const prefix = '.'
 
 
-const ownerNumber =['94743012546']
+const ownerNumber =['94727864657']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
